b39f1c9c05f355a7aa328b88d86b372bce9ae6ff
zalatin <hfzeng@foxmail.com>
2020-11-10 18:11:53 +0800

    求取节点向量

--- BSplineCurSurf.py
+++ BSplineCurSurf.py
@@ -1,5 +1,6 @@
 from math import *
 import logging
+import numpy
 
 logging.basicConfig(level=logging.INFO)
 
@@ -15,20 +16,27 @@ class BSplineCurSurf(object):
         # 第i个型值点 Q[i]=[x,y,z] & 第i行j列的型值点 Q[i][j]=[x,y,z]
         temp = [0 for i in range(n-1)] # n个型值点，n-1段弦长
         for i in range(n-1):
-            temp[i] = sqrt(pow(Q[i+1][1]-Q[i][1],2)+pow(Q[i+1][2]-Q[i][2],2)+pow(Q[i+1][3]-Q[i][3],2))
+            logging.info('i now is %s' % (i+1))
+            temp[i] = sqrt(pow(Q[i+1][0]-Q[i][0],2)+pow(Q[i+1][1]-Q[i][1],2)+pow(Q[i+1][2]-Q[i][2],2))
         sumtemp = sum(temp) # 顺序相邻两型值点之间距离的和，即总弦长
         for i in range(p+1): # 前p+1个节点为0
             U[i] = 0
-        for i in range((n+2)+1, n+p+3): # 后p+1个节点为1
+        for i in range(n+2, n+p+3): # 后p+1个节点为1
             U[i] = 1
-        for i in range(p+1, n+p-2):
-            U[i+1] = U[i] + temp[i-p] / sumtemp
+        for i in range(p+1, n+p-1):
+            U[i] = U[i-1] + temp[i-p-1] / sumtemp # 内节点
         return U
     
 # 测试
 if __name__ == '__main__':
-    a = 5
-    logging.info('hello %s' % a)
+    pt = numpy.loadtxt('data2.txt') # 下载型值点数据
+    Q = pt.tolist() # 把numpy变为list
+    logging.info(Q[2])
+    BS = BSplineCurSurf(3)
+    U = BS.paraAndNodVect(Q)
+    logging.info(U)
+
+
 
 
         
